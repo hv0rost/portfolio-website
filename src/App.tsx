6f2c23d935c46c48bc95ffe2d366b2f0530dfe06
Daniil Matyash <daniil.matyash@aisa.ru>
2024-05-15 18:15:59 +0300

    fix overflow bug

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Contacts from './components/Contacts'
 
 export default function App() {
 	return (
-		<div className="overflow-x-hidden text-neutral-300 antialised">
+		<div className="overflow-x-hidden overflow-y-hidden text-neutral-300 antialised">
 			<div className="fixed top-0 -z-10 h-full w-full">
 				<div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
 			</div>
